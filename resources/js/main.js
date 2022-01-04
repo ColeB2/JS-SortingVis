09ab2235ee8aa379df2dab57476b1ad35fff590d
Cole <cebCole@gmail.com>
2022-01-04 06:50:08 -0600

    removed unused functions and code copied from GOL

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,3 @@
 
 
 //Delay Range Slider
-function gameDelay() {
-	delayOutput.innerHTML = this.value;
-	mainGame.delay = this.value;	
-}
-
-const delayRange = document.getElementById('delayRange');
-const delayOutput = document.getElementById('delayValue');
-delayOutput.innerHTML = delayRange.value;
-delayRange.addEventListener('input', gameDelay, false)
