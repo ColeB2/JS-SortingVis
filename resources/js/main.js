087c42cef771cc80bc9ece353d15b2fc9cc48638
Cole <cebCole@gmail.com>
2022-01-05 06:33:15 -0600

    constants.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,10 @@
+import * as cons from './constants.js'
+
+const ARRAY_LENGTH = 20
+Array.from(Array(ARRAY_LENGTH)).map(x=>Math.random())
 
 
-//Delay Range Slider
+TEST_ARRAY = [100,25,95,15,48,16,20,17,55,69,72,83,14,28,30,22,78,69,38,45,55]
+
+const CANVAS = document.getElementById("myCanvas")
+const CTX = CANVAS.get
