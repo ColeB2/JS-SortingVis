bc16626955fd522042b5ef204657b29fedf0bda7
Cole <cebCole@gmail.com>
2022-01-10 07:53:58 -0600

    > --> >=

--- resources/js/main.js
+++ resources/js/main.js
@@ -25,9 +25,8 @@ function updateCanvas(arr, context) {
 }
 
 
-
 function bubbleSort(arr) {	
-	for (let i = arr.length - 1; i > 0; i--) {
+	for (let i = arr.length - 1; i >= 0; i--) {
 		for (let j = 0; j < i; j++) {
 			if (arr[j] > arr[j+1]) {
 				let temp = arr[j]
