4f6bf5040e766d3c7041bb868ea163227fefb0b5
Cole <cebCole@gmail.com>
2022-01-10 07:45:22 -0600

    removed improper ;

--- resources/js/main.js
+++ resources/js/main.js
@@ -29,12 +29,12 @@ function updateCanvas(arr, context) {
 function bubbleSort(arr) {
 	n = arr.length
 	
-	for (let i = arr.length - 1; i > 0; i--;) {
-		for (let j = 0; j < i; j++;) {
+	for (let i = arr.length - 1; i > 0; i--) {
+		for (let j = 0; j < i; j++) {
 			if (arr[j] > arr[j+1]) {
-				let temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;			
+				let temp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp			
 			}
 		}      		
 	}
