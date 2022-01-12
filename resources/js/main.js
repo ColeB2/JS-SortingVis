78c759bc3defcfe35cddfbc1d56d6836df93e819
Cole <cebCole@gmail.com>
2022-01-12 07:47:05 -0600

    removed unuse bubble recursion sorts

--- resources/js/main.js
+++ resources/js/main.js
@@ -41,38 +41,6 @@ function bubbleSort(arr) {
 	}
 }
 
-function bubbleSortR(arr, n) {
-	if (n==1) {
-		return
-	}
-	
-	for (var i = 0; i < n - 1; i++)
-        if (arr[i] > arr[i + 1])
-        {
-         
-            // swap arr[i], arr[i+1]
-            var temp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = temp;
-        }
-	bubbleSort(arr, n-1)
-}
-function bubbleDriver(arr) {
-	bubbleSortR(arr, arr.length)
-}
-
-function pauseLoop() {
-	if (isRunning) {
-		pauseButton.innerText = 'Start';
-		pauseButton.classList.remove('button-paused')
-	} else {
-		pauseButton.innerText = 'Pause';
-		pauseButton.classList.add('button-paused')
-	}
-	
-	isRunning = !isRunning;
-}
-
 const pauseButton = document.getElementById('pause')
 pauseButton.addEventListener('click', pauseLoop, false)
 
