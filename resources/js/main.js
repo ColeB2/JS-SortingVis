e5ba6663bf2eea224d515e36b7f015e861cb379b
Cole <cebCole@gmail.com>
2022-01-13 07:50:34 -0600

    commented out sort update canvas

--- resources/js/main.js
+++ resources/js/main.js
@@ -39,7 +39,7 @@ function bubbleSort(arr) {
 	for (let i = arr.length - 1; i >= 0; i--) {
 		for (let j = 0; j < i; j++) {
 			if (arr[j] > arr[j+1]) {
-				updateCanvas(arr, cons.CTX)
+				//updateCanvas(arr, cons.CTX)
 				console.log(arr)
 				let temp = arr[j]
 				arr[j] = arr[j+1]
