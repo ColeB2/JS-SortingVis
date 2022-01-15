44088c4d8ae58f984c93e36567cc94e4a81c9398
Cole <cebCole@gmail.com>
2022-01-15 05:45:42 -0600

    ain2

---
+++ resources/js/main2.js
@@ -0,0 +1,138 @@
+
+
+import * as cons from './constants.js'
+
+const ARRAY_LENGTH = 20
+Array.from(Array(ARRAY_LENGTH)).map(x=>Math.random())
+
+
+const TEST_ARRAY = [100,25,95,15,48,16,20,17,55,69,72,83,14,28,30,22,78,69,38,45,56]
+
+
+
+//TEST_ARRAY.map(function(element, index) {
+//	cons.CTX.fillRect( ((index * cons.BAR_WIDTH)+10), 0, cons.BAR_WIDTH-1, (2*element))
+//});
+
+
+function drawArray(arr, context) {
+	context.fillStyle = '#343A40'
+	arr.map(function(element, index) {
+		context.fillSt
+		context.fillRect( ((index * cons.BAR_WIDTH)+10), 0, cons.BAR_WIDTH-1, (2*element))
+	})
+}
+
+function updateLogic(arr, context) {
+	
+}
+
+function updateVisuals(arr, context) {
+	context.clearRect(0, 0, cons.CANVAS_WIDTH, cons.CANVAS_HEIGHT);
+	drawArray(arr, context)
+	
+}
+
+async function updateCanvas(arr, context) {
+	updateVisuals(arr, context)
+	await delay()
+}
+
+function* bubbleSort(arr) {
+	algoRunning = true
+	for (let i = arr.length - 1; i >= 0; i--) {
+		for (let j = 0; j < i; j++) {
+			if (arr[j] > arr[j+1]) {
+				let temp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+				yield;
+			}
+		}      		
+	}
+	algoRunning = false
+}
+
+
+async function bubbleSort(arr) {
+	algoRunning = true
+	for (let i = arr.length - 1; i >= 0; i--) {
+		for (let j = 0; j < i; j++) {
+			if (arr[j] > arr[j+1]) {
+				let temp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+				
+				//remove and abstract the update, pause portions.
+				await pause()
+				await updateCanvas(arr, cons.CTX)
+			}
+		}      		
+	}
+	algoRunning = false
+}
+
+async function delay() {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve();
+		}, 1000)
+	})
+}
+
+async function pause() {
+	return new Promise((resolve) => {
+		if (isRunning) {
+			resolve()
+		}
+		
+	})
+}
+
+function pauser() {
+	console.log('inside pauser')
+	if (isRunning) {
+		return
+	}
+	return delay().then(() => pauser());
+}
+
+
+async function pauseLoop() {
+	console.log("pauseLoop: isRunning:")
+	console.log(isRunning)
+	if (isRunning) {
+		pauseButton.innerText = 'Start';
+		pauseButton.classList.remove('button-paused')
+		//await pause()
+		pauser()
+	} else {
+		pauseButton.innerText = 'Pause';
+		pauseButton.classList.add('button-paused')	
+	}
+	
+	isRunning = !isRunning;
+	mainLoop();
+}
+
+const pauseButton = document.getElementById('pause')
+pauseButton.addEventListener('click', pauseLoop, false)
+
+
+async function mainLoop() {
+	if (isRunning) {
+		//await pause()
+		pauser()
+		if (!algoRunning) {
+			await bubbleSort(TEST_ARRAY)
+		}
+		
+	}
+}
+
+updateCanvas(TEST_ARRAY, cons.CTX);
+let isRunning = false
+let algoRunning = false;
+mainLoop();
+//bubbleSort(TEST_ARRAY)
+//animate()
