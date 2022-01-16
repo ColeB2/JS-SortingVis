4e41e63568a1e1258978501909bab7d188eaca8a
Cole <cebCole@gmail.com>
2022-01-16 05:35:18 -0600

    debugging console logs

--- resources/js/main2.js
+++ resources/js/main2.js
@@ -53,8 +53,11 @@ function* bubbleSort(arr) {
 
 
 function generatorRunner(generatorObj) {
+	console.log(generatorObj)
 	for (let value of generatorObj) {
+		console.log("inside for loop genobj value of")
 		console.log(value)
+
 	}
 }
 
