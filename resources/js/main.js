498f287a962a7fcae2f45136465d3f52b3fe965b
Cole <cebCole@gmail.com>
2022-01-20 07:11:14 -0600

    moved bubble to separate algorithm file

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,11 @@
 import * as cons from './constants.js'
+import {bubbleSort} from './algorithms.js'
 
 const ARRAY_LENGTH = 20
 Array.from(Array(ARRAY_LENGTH)).map(x=>Math.random())
 
 
 const TEST_ARRAY = [100,25,95,15,48,16,20,17,55,69,72,83,14,28,30,22,78,69,38,45,56]
-
-
-
-//TEST_ARRAY.map(function(element, index) {
-//	cons.CTX.fillRect( ((index * cons.BAR_WIDTH)+10), 0, cons.BAR_WIDTH-1, (2*element))
-//});
 
 
 function drawArray(arr, context) {
@@ -52,23 +47,6 @@ function updateVisuals(arr, context, choice=null, arr2=[]) {
 
 function updateCanvas(arr, context, choice=null, arr2=[]) {
 	updateVisuals(arr, context, choice, arr2)
-}
-
-function* bubbleSort(arr) {
-	algoRunning = true
-	for (let i = arr.length - 1; i >= 0; i--) {
-		for (let j = 0; j < i; j++) {
-			yield [arr, "compare", [j, j+1]]
-			if (arr[j] > arr[j+1]) {
-				yield [arr, "swap", [j, j+1]];
-				let temp = arr[j]
-				arr[j] = arr[j+1]
-				arr[j+1] = temp
-				yield [arr, "swap", [j, j+1]];
-			}
-		}      		
-	}
-	algoRunning = false
 }
 
 
@@ -113,10 +91,10 @@ function mainLoop() {
 	
 }
 
+
+//Main
 updateCanvas(TEST_ARRAY, cons.CTX);
 let isRunning = false
 let algoRunning = false;
 var generatorAlgo = null;
 mainLoop();
-//bubbleSort(TEST_ARRAY)
-//animate()
