5b024250b0dca0b272c2bc4d515bdc4bb3ad6a8f
Cole <cebCole@gmail.com>
2022-01-25 07:51:35 -0600

    test

--- resources/js/main.js
+++ resources/js/main.js
@@ -43,6 +43,7 @@ function generateArray() {
 	if (!isRunning) {
 		TEST_ARRAY = Array.from({length: ARRAY_LENGTH}, () => Math.ceil(Math.random() * 200));
 		updateCanvas(TEST_ARRAY, cons.CTX);
+		selectionSortButton.addEventListener('click', (event) => sortFunction(selectionSortButton, selectionSort, TEST_ARRAY), false)
 	}
 }
 
