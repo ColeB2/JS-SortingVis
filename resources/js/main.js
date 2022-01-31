66c58deecaea9b706ac8c7416b3d150cc57eb6a8
Cole <cebCole@gmail.com>
2022-01-31 07:42:52 -0600

    removed console log debugs which were fixed by removing a ;

--- resources/js/main.js
+++ resources/js/main.js
@@ -133,10 +133,7 @@ function mainLoop() {
 	function main() {
 		if (myGlobal.isRunning) {
 			if (myGlobal.generatorAlgo !== null) {
-				console.log(myGlobal.generatorAlgo)
-				console.log(myGlobal.algoSelected)
 				let algo_results = myGlobal.generatorAlgo.next()
-				console.log(algo_results)
 				let val = algo_results['value']
 				let done = algo_results['done']
 				if (!done) {
