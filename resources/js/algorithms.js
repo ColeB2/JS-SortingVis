00f749e47a758c7e404fa0f1b6f8b58adcbdc65b
Cole <cebCole@gmail.com>
2022-02-02 07:33:35 -0600

    testing completed array slice selectionSort

--- resources/js/algorithms.js
+++ resources/js/algorithms.js
@@ -17,25 +17,26 @@ export function* bubbleSort(arr) {
 }
 
 export function* selectionSort(arr) {
-	for (let i = arr.length-1; i >= 0; i--) {
+	for (let i = arr.length - 1; i >= 0; i--) {
+		let completed_arr = arr.slice(0,i+1)
 		var maxI = 0
-		yield [arr, [maxI],[],[]]
+		yield [arr, [maxI],[],[], completed_arr]
 		
 		for (let j = 1; j <= i; j++) {
-			yield [arr, [maxI, j],[],[]]
+			yield [arr, [maxI, j],[],[], completed_arr]
 			
 			if (arr[j] > arr[maxI]) {
 				var maxI = j
-				yield [arr, [maxI],[],[]]
+				yield [arr, [maxI],[],[], completed_arr]
 			}
 		}
-		yield [arr, [],[i, maxI],[]]
+		yield [arr, [],[i, maxI],[], completed_arr]
 		
 		let temp = arr[i]
 		arr[i] = arr[maxI]
 		arr[maxI] = temp
 		
-		yield [arr, [],[i, maxI],[]]
+		yield [arr, [],[i, maxI],[], completed_arr]
 	}
 }
 
