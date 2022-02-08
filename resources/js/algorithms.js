1adc3e0e0d7a4f77a06a47a876c802375b501846
Cole <cebCole@gmail.com>
2022-02-08 07:58:31 -0600

    bug fix

--- resources/js/algorithms.js
+++ resources/js/algorithms.js
@@ -3,13 +3,13 @@
 export function* bubbleSort(arr) {
 	for (let i = arr.length - 1; i >= 0; i--) {
 		for (let j = 0; j < i; j++) {
-			yield [arr, [j, j+1],[] ,[], [0, i+1], [arr[0], arr[1]  ]
+			yield [arr, [j, j+1],[] ,[], [0, i+1], [arr[0], arr[1]]  ];
 			if (arr[j].Value > arr[j+1].Value) {
-				yield [arr, [], [j, j+1], [], [0, i+1], [arr[0], arr[1]  ];
+				yield [arr, [], [j, j+1], [], [0, i+1], [arr[0], arr[1]]  ];
 				let temp = arr[j]
 				arr[j] = arr[j+1]
 				arr[j+1] = temp
-				yield [arr, [], [j, j+1], [], [0, i+1], [arr[0], arr[1]  ];
+				yield [arr, [], [j, j+1], [], [0, i+1], [arr[0], arr[1]]  ];
 			}
 		}      		
 	}
