f6cd318e2f5e1a983fb92e64bb8e27fa7b27f953
Cole <cebCole@gmail.com>
2022-02-10 05:07:23 -0600

    test

--- resources/js/algorithms.js
+++ resources/js/algorithms.js
@@ -87,7 +87,7 @@ export function* mergeSort(arr) {
 			var displayArr = leftSubArr.concat(rightSubArr)
 			yield [arr, [], [], [], displayArr]
 			while (i < leftN && j < rightN) {
-				yield [arr, [leftSubArr[i], rightSubArr[j]], [], [arr[k]], displayArr]
+				//yield [arr, [leftSubArr[i], rightSubArr[j]], [], [arr[k]], displayArr]
 				if (leftSubArr[i].Value <= rightSubArr[j].Value) {
 					yield [arr, [arr[k], arr[i]], [], [], displayArr]
 					yield [arr, [], [arr[k], arr[i]], [], displayArr]
