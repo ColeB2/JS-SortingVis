1cddbce5a4dd66b0cfad5f369ee47da2a8fe049f
Cole <cebCole@gmail.com>
2022-02-10 05:10:51 -0600

    replaced wrongfully removed index

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,7 +4,7 @@ import {bubbleSort, insertionSort, mergeSort, selectionSort} from './algorithms.
 
 function updateCanvas(arr, context, compare=[], swap=[], focusElem=[], arrObj=[]) {
 	context.clearRect(0, 0, cons.CANVAS_WIDTH, cons.CANVAS_HEIGHT);
-	arr.map(function(element) {
+	arr.map(function(element, index) {
 		if (compare.includes(element)) {
 			context.fillStyle = '#89FB92'
 		} else if (swap.includes(element)) {
