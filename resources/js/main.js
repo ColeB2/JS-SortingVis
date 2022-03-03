45552b6ebadb8c0d9dac7c8ee92769e9329aa1d9
Cole <cebCole@gmail.com>
2022-03-03 07:01:38 -0600

    debug

--- resources/js/main.js
+++ resources/js/main.js
@@ -98,6 +98,7 @@ function changeSlider() {
 	console.log(event.target)
 	console.log(event.target.id)
 	event.target.innerHTML = this.value;
+	console.log(myGlobal.sliderValues[event.target.id]["htmlOutput"].innerHTML)
 	myGlobal.sliderValues[event.target.id]["htmlOutput"].innerHTML = this.value
 	myGlobal.sliderValues[event.target.id] = this.value;
 	
