1207e7fabc952ba87dd8c278b3a5e2bd733e1bb3
Cole <cebCole@gmail.com>
2022-03-05 07:25:37 -0600

    debugs

--- resources/js/algorithms.js
+++ resources/js/algorithms.js
@@ -355,43 +355,6 @@ export function* quickSort(arr) {
 	yield [arr]
 }
 //////////////////INTRO SORT 
-function* introSortQuick(arr, left, right, depth) {
-	let displayArr = arr.slice(left, right+1)
-	if (left < right && depth > 0) {
-		let i = left - 1
-		let pivot = arr[right] // last element as pivot
-		yield [arr, [],[], [pivot, arr[i]], displayArr]
-		for (let j = left; j < right; j++) {
-			yield [arr, [arr[j], pivot], [], [arr[i+1]], displayArr]
-			if (arr[j].Value < pivot.Value) {
-				i++;
-				
-				if (i == j) {
-					continue
-				}
-				yield [arr, [],[arr[i], arr[j]],[],displayArr]
-				let temp = arr[i]
-				arr[i] = arr[j]
-				arr[j] = temp
-				yield [arr, [],[arr[i], arr[j]],[],displayArr]
-			}
-		}
-		
-		if (i+1 != right) {
-			yield [arr, [],[arr[i+1], arr[right]],[],displayArr]
-			let temp = arr[i+1]
-			arr[i+1] = arr[right]
-			arr[right] = temp
-			yield [arr, [],[arr[i+1], arr[right]],[],displayArr]
-		}
-			
-		let pivot_index = i + 1
-		
-		yield* introSortQuick(arr, left, pivot_index-1, depth - 1)
-		yield* introSortQuick(arr, pivot_index+1, right, depth - 1)
-		
-	}	
-}
 function* partition(arr, left, right) {
 	let i = left - 1
 	let pivot = arr[right] // last element as pivot
@@ -435,10 +398,12 @@ function* introSortHelper(arr, maxDepth, leftRight=[null,null]) {
 		console.log("Heap Sort")
 		yield* heapSort(arr)
 	} else {
+		console.log("Quick Partition")
 		//Partition portioning:
 		let i = left - 1
 		let pivot = arr[right] // last element as pivot
 		// yield [arr, [],[], [pivot, arr[i]], displayArr]
+		console.log(i, pivot, left, right)
 		for (let j = left; j < right; j++) {
 			// yield [arr, [arr[j], pivot], [], [arr[i+1]], displayArr]
 			if (arr[j].Value < pivot.Value) {
